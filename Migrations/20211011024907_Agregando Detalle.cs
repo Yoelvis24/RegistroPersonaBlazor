73bf5b73e2c046951221eee8fa9c34a7a5cd1697
Yoelvis Perez <yoelvis.perez.20017@gmail.com>
2021-10-10 22:50:02 -0400

    Agregando nueva Migración

---
+++ Migrations/20211011024907_Agregando Detalle.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroBlazor.Migrations
+{
+    public partial class AgregandoDetalle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
