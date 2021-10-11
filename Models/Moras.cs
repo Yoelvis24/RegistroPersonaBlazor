71e9b966fb6ddb2a347a9bead32f8d06aa86d622
Yoelvis Perez <yoelvis.perez.20017@gmail.com>
2021-10-10 22:28:58 -0400

    Agregando Entidad Moras y MorasDetalle

---
+++ Models/Moras.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroBlazor.Models
+{
+    public class Moras
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Total { get; set; }
+
+        public virtual ICollection<MorasDetalle> MorasDetalle { get; set; }
+
+        public Moras()
+        {
+            MoraId = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            MorasDetalle = new HashSet<MorasDetalle>();
+        }
+    }
+}
---
+++ Models/MorasDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroBlazor.Models
+{
+    public class MorasDetalle
+    {
+        [Key]
+        public int MoraDetalleId { get; set; }
+        public int MoraId { get; set; }
+        public Moras Mora { get; set; }
+        public int PrestamoId { get; set; }
+        public Prestamos Prestamo { get; set; }
+        public double Valor { get; set; }
+
+        public MorasDetalle()
+        {
+            MoraDetalleId = 0;
+            MoraId = 0;
+            PrestamoId = 0;
+            Valor = 0;
+        }
+    }
+}
