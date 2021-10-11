43ad617ec2f6fc488c6cce992174697b6401a4ad
Yoelvis Perez <yoelvis.perez.20017@gmail.com>
2021-10-10 22:44:43 -0400

    Agregando nueva Migración (AgregandoDetalle)

--- Migrations/20211011023848_Agregando Detalle.Designer.cs
+++ Migrations/20211011023848_Agregando Detalle.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroBlazor.DAL;
 
 namespace RegistroBlazor.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20211011023848_Agregando Detalle")]
+    partial class AgregandoDetalle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace RegistroBlazor.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 1 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 2 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 3 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 4 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 5 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 6 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 7 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 8 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 9 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using RegistroBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\yoelvis\source\repos\RegistroBlazor\_Imports.razor"
+#line 10 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\_Imports.razor"
 using RegistroBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\yoelvis\source\repos\RegistroBlazor\Pages\FetchData.razor"
+#line 3 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\Pages\FetchData.razor"
 using RegistroBlazor.Data;
 
 #line default
@@ -98,7 +98,7 @@ using RegistroBlazor.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\yoelvis\source\repos\RegistroBlazor\Pages\FetchData.razor"
+#line 39 "C:\Users\yoelvis\source\repos\Yoelvis24\RegistroDetalleBlazor\RegistroBlazor\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
